083e4ee3478e1bba7f9d0c61a923d720c4152908
Travis Cline <travis.cline@gmail.com>
2017-06-17 13:49:14 -0700

    filedrop: add basic file dropping example

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+func init() {
+	// This is needed to arrange that main() runs on main thread.
+	// See documentation for functions that are only allowed to be called from the main thread.
+	runtime.LockOSThread()
+}
+
+func main() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+	window.SetDropCallback(func(w *glfw.Window, names []string) {
+		fmt.Println(names)
+	})
+
+	for !window.ShouldClose() {
+		// Do OpenGL stuff.
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
